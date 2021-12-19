4548d29513f0b153ea856dc2734a55e870f83f6a
abdoessordo <abdoessordo01@gmail.com>
2021-12-19 11:30:16 +0100

    Groupe11

---
+++ Groupe11/script.js
@@ -0,0 +1,82 @@
+let formBtn = document.querySelector('#a');
+let contactForm = document.querySelector('.contact-form-container');
+let formClose = document.querySelector('#form-close');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+formBtn.addEventListener('click', () =>{
+    contactForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    contactForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    })
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".photo-slider", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".photo-slider2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+var swiper = new Swiper(".services-slider", {
+    spaceBetween: -215,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
